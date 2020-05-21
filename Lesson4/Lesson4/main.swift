b30b38be20e6d0ad540784b59af8046dce8a0189
EgorkaJAred <egorkajared@gmail.com>
2020-05-22 01:14:39 +0300

    Структура 4 урока

---
+++ Lesson4/Lesson4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson4
+//
+//  Created by Егор Ненастьев on 22.05.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
