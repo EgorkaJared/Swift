9867b8b12695057c4d9a899344a366b3e2f3c144
EgorkaJAred <egorkajared@gmail.com>
2020-05-25 11:41:41 +0300

    структрура 5 урока

---
+++ Lesson5/Lesson5/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson5
+//
+//  Created by Егор Ненастьев on 25.05.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
