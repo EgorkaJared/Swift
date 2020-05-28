9eb555597bff559910eb3f16442468848b6c4e56
EgorkaJAred <egorkajared@gmail.com>
2020-05-28 19:47:01 +0300

    структура 6 урока

---
+++ Lesson6/Lesson6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson6
+//
+//  Created by Егор Ненастьев on 28.05.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
