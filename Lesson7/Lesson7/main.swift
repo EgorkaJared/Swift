98f6bf96e03fa5e9f004f66ef9d3aa8ccf415721
EgorkaJAred <egorkajared@gmail.com>
2020-06-04 11:26:32 +0300

    Структура 7  урока

---
+++ Lesson7/Lesson7/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson7
+//
+//  Created by Егор Ненастьев on 04.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
