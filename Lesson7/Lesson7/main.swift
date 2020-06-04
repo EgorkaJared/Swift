ce7cdd0a1ef0619072d9d77991888b26957f08f4
EgorkaJAred <egorkajared@gmail.com>
2020-06-04 16:00:14 +0300

    сабскрипты

--- Lesson7/Lesson7/main.swift
+++ Lesson7/Lesson7/main.swift
@@ -8,45 +8,81 @@
 
 import Foundation
 
+protocol Name {
+    var name: Fio { get set }
+    var zp : ZP { get set }
+    
+}
+
 enum Fio {
     case Иванов, Петров, Мясников, Полякова, Травник
 }
-enum ZP: Int {
+enum ZP: Double {
     case hight = 300000
     case middle = 200000
     case low = 100000
 }
 
 
-class Groop {
-    var vedomost = [Fio: ZP]()
+class Groop : Name {
+    var name: Fio
+    var zp: ZP
     
-    init(vedeomst:[Fio: ZP]) {
-          self.vedomost = vedeomst
+    init(name:Fio, zp: ZP) {
+        self.name = name
+        self.zp = zp
       }
     
-    func solo (ofFio: Fio) -> ZP.RawValue? {
-        return vedomost[ofFio].map { $0.rawValue }
+//    func solo (ofFio: Fio) -> ZP.RawValue? {
+//        return vedomost[ofFio].map { $0.rawValue }
+//    }
+//
+//    func average() -> Double {
+//        var sumSalary: Int = 0
+//        for ind in vedomost {
+//            sumSalary = sumSalary + ind.value.rawValue
+//        }
+//        return Double(sumSalary/vedomost.count)
+//    }
+}
+
+struct Staff<T:Name> {
+    private var massive: [T] = []
+    mutating func pushMassive(_ newStaff : T) {
+        massive.append(newStaff)
     }
     
-    func average() -> Double {
-        var sumSalary: Int = 0
-        for ind in vedomost {
-            sumSalary = sumSalary + ind.value.rawValue
+    subscript (ofFio: Fio) -> ZP.RawValue? {
+        let soloZP = massive.first {$0.name == ofFio}
+        return soloZP?.zp.rawValue
+       }
+    subscript (allZp: [Fio]) -> Double {
+        var sumZp : Double = 0
+        for ind in allZp {
+            for idX in massive {
+                if ind == idX.name {
+                    sumZp = Double(sumZp + idX.zp.rawValue)
+                }
+            }
         }
-        return Double(sumSalary/vedomost.count)
+        return sumZp
     }
+   
 }
 
-struct Staff<T> {
-    private var massive: [T] = []
-    func pushMassive(_ : T) -> [T] {
-        <#function body#>
-    }
-}
+//let one = Groop(vedeomst: [.Иванов : .hight
+//    , .Мясников : .low , .Полякова : .middle])
 
-let one = Groop(vedeomst: [.Иванов : .hight
-    , .Мясников : .low , .Полякова : .middle])
+//print (one.solo(ofFio: .Иванов)!)
+//print (one.average())
 
-print (one.solo(ofFio: .Иванов)!)
-print (one.average())
+var StaffStack = Staff<Groop>()
+
+StaffStack.pushMassive(Groop(name: .Иванов, zp: .hight))
+StaffStack.pushMassive(Groop(name: .Мясников, zp: .low))
+StaffStack.pushMassive(Groop(name: .Полякова, zp: .middle))
+
+print(StaffStack)
+
+print(StaffStack[.Иванов]!)
+print(StaffStack[[.Иванов,.Мясников,.Травник]])
