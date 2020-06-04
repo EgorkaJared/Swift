43dc54c4308b607f761dee749eacb05cb9fc0ea3
EgorkaJAred <egorkajared@gmail.com>
2020-06-04 12:09:51 +0300

    первый класс

--- Lesson7/Lesson7/main.swift
+++ Lesson7/Lesson7/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Fio {
+    case Иванов, Петров, Мясников, Полякова, Травник
+}
+enum ZP: Int {
+    case hight = 300000
+    case middle = 200000
+    case low = 100000
+}
 
+
+class Groop {
+    var vedomost = [Fio: ZP]()
+    
+    func solo (ofFio: Fio) -> ZP.RawValue? {
+        return vedomost[ofFio].map { $0.rawValue } ?? 0
+    }
+    init(vedeomst:[Fio: ZP]) {
+        self.vedomost = vedeomst
+    }
+    
+}
+
+let one = Groop(vedeomst: [.Иванов : .hight
+    , .Мясников : .low , .Полякова : .middle])
+
+print (one.solo(ofFio: .Петров)!)
