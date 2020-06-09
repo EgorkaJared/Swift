45ca009998b7dd83d5fa3397457b38a080847b83
EgorkaJAred <egorkajared@gmail.com>
2020-06-09 17:07:00 +0300

    скушать хвост

---
+++ Lesson8/Lesson8/SnakeHead2.swift
@@ -0,0 +1,13 @@
+//
+//  SnakeHead2.swift
+//  Lesson8
+//
+//  Created by Егор Ненастьев on 09.06.2020.
+//  Copyright © 2020 Егор Ненастьев. All rights reserved.
+//
+
+import UIKit
+
+class SnakeHead2: NSObject {
+
+}
